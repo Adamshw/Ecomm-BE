5c6bbd96c0fb5be3d259932539014ad135a18445
adamshw <adam.shwehne@gmail.com>
2023-08-07 19:03:57 +0200

    added navbar

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,8 +1,9 @@
 import { redirect } from 'next/navigation';
 import { auth } from '@clerk/nextjs';
 
-import Navbar from '@/components/navbar' 
+import Navbar from '@/components/navbar'
 import prismadb from '@/lib/prismadb';
+
 
 export default async function DashboardLayout({
   children,
@@ -29,10 +30,6 @@ export default async function DashboardLayout({
   };
 
   return (
-    <>
-      <Navbar />
-      <div>This will be a Navbar</div>
-      {children}
-    </>
+    <Navbar />
   );
 };
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,17 @@
+import { UserButton } from "@clerk/nextjs";
+import { MainNav } from "./main-nav";
+
 const Navbar = () => {
     return (
-        <div>
-
+        <div className="border-b">
+            <div className="flex h-16 items-center px-4">
+                {/* <StoreSwitcher items={stores} /> */}
+                <MainNav className="mx-6" />
+                <div className="ml-auto flex items-center space-x-4">
+                    {/* <ThemeToggle /> */}
+                    <UserButton afterSignOutUrl="/" />
+                </div>
+            </div>
         </div>
     );
 }
