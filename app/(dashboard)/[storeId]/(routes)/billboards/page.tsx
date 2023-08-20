a1abcd5d0c27e115fe4bd0682e3c964f48b20f79
adamshw <adam.shwehne@gmail.com>
2023-08-20 14:19:28 +0200

    added prisma billboard schema and page component

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,12 @@
+const BillboardsPage = () => {
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                Billboard page
+                {/* <BillboardForm initialData={billboard} /> */}
+            </div>
+        </div>
+    );
+}
+
+export default BillboardsPage;
