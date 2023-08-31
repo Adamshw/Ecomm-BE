30cbaa47267a4d802ae63ad860c7a357dcdbe130
adamshw <adam.shwehne@gmail.com>
2023-08-31 20:39:35 +0200

    update

--- app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/columns.tsx
@@ -2,19 +2,25 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 
+import { CellAction } from "./cell-action"
+
 export type BillboardColumn = {
-  id: string
-  label: string
-  createdAt: string
+    id: string
+    label: string
+    createdAt: string
 }
 
 export const columns: ColumnDef<BillboardColumn>[] = [
-  {
-    accessorKey: "label",
-    header: "Label",
-  },
-  {
-    accessorKey: "createdAt",
-    header: "Date",
-  }
+    {
+        accessorKey: "label",
+        header: "Label",
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Date",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
+    }
 ]
